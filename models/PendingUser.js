b5b85a85fe16d39044dff11fbad9d13bb00eb096
cblnam412 <23520979@gm.uit.edu.vn>
2025-10-27 17:44:37 +0700

    Sửa lỗi đăng kí

--- models/PendingUser.js
+++ models/PendingUser.js
@@ -1,16 +1,51 @@
 ﻿import mongoose from "mongoose";
 
-// lưu tạm thông tin của user trong lúc đợi xác nhận OTP
 const pendingUserSchema = new mongoose.Schema({
-    full_name: String,
-    email: String,
-    phone_number: String,
-    studentId: String,
-    DOB: Date,
-    password: String,
-    address: String,
-    enrollment_year: Number,
-    faculty: String,
+    full_name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    phone_number: {
+        type: String,
+        unique: true,
+        sparse: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        sparse: true,
+        lowercase: true,
+        trim: true,
+    },
+    studentId: {
+        type: String,
+        unique: true,
+        trim: true,
+    },
+    DOB: {
+        type: Date,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        default: null,
+        trim: true,
+    },
+    enrollment_year: {
+        type: Number,
+        min: 1900,
+        max: 2100,
+    },
+    faculty: {
+        type: String,
+        enum: ["IS", "SE", "CS", "CE"],
+        required: true,
+    },
     otp: String,
     expiresAt: { type: Date, index: { expires: 600 } }
 });
