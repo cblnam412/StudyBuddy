a8876dc2aacf352c1f08bec90d44035bd347a60c
cari <anhthuchevu@gmail.com>
2025-11-28 02:42:26 +0700

    note: notification component is waiting for backend API

--- client/src/screens/notifications/notificationListPage.jsx
+++ client/src/screens/notifications/notificationListPage.jsx
@@ -4,8 +4,6 @@ import './notifications.css';
 const NotificationListPage = () => {
   return (
     <div className="notification-page-mock-center">
-        <h1>Trang Kiểm tra UI Thông Báo</h1>
-        <p>Đây là nơi đặt component NotificationDropdown.</p>
         <div className="mock-dropdown-placement">
             <NotificationDropdown />
         </div>
