14f74e35ac15ed24c488457d1b0267e574329187
cblnam412 <23520979@gm.uit.edu.vn>
2025-12-11 20:47:52 +0700

    Thêm lấy, duyệt đơn xin làm mod

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getUserStatsByRole, setRole, getOnlineUsersCount, getRoomStatsByStatus,
-    getReportProcessingRatio
+    getReportProcessingRatio, findModeratorApplications, approveModeratorApplication, rejectModeratorApplication
 } from "../controllers/adminController.js";
 import { verifyToken, isAdmin } from "../middlewares/authMiddleware.js";
 
@@ -11,5 +11,9 @@ router.get("/get-user", verifyToken, isAdmin, getUserStatsByRole);
 router.get("/get-user-online", verifyToken, isAdmin, getOnlineUsersCount);
 router.get("/get-room", verifyToken, isAdmin, getRoomStatsByStatus);
 router.get("/report-ratio", verifyToken, isAdmin, getReportProcessingRatio);
+
+router.get("/moderator-applications", verifyToken, isAdmin, findModeratorApplications);
+router.post("/moderator-applications/:id/approve", verifyToken, isAdmin, approveModeratorApplication);
+router.post("/moderator-applications/:id/reject", verifyToken, isAdmin, rejectModeratorApplication);
 
 export default router;
