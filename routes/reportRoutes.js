73e7fc2edc55d07dcc1075ca8fc315dc4d52802b
phanthingochan <ngochan2005blislife@gmail.com>
2025-12-13 09:47:55 +0700

    fix bug cannot get reviewerId for processing report

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isModerator, verifyToken } from "../middlewares/authMiddleware.js";
-import { createReport, reviewReport, processReport, findReport, viewReportDetails } from "../controllers/reportController.js";
+import { createReport, reviewReport, processReport, findReport, viewReportDetails, rejectReport } from "../controllers/reportController.js";
 import { isRoomLeader } from "../middlewares/roomMiddleware.js";
 
 const router = express.Router();
@@ -9,6 +9,7 @@ router.post("/", verifyToken, createReport);
 router.get("/", verifyToken, isModerator, findReport);
 router.get("/:id", verifyToken, isModerator, viewReportDetails);
 router.post("/:id/review", verifyToken, isModerator, reviewReport);
+router.post("/:id/reject", verifyToken, isModerator, rejectReport);
 router.post("/:id/process", verifyToken, isModerator, processReport);
 
 export default router;
